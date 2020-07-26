66011cbf5216e8f6fb3f2e463097bebd60da84cf
MatKabal <mateuslima200919@gmail.com>
2020-07-26 12:08:52 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Meu primeiro grande projeto");
+		
+		Position pos = new Position(5, 3);
+		
+		System.out.println(pos);
 		
 	}
 
